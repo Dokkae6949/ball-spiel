f252c750b1664817d43855626e89267cda3f2400
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-09-29 14:45:10 +0200

    add player
    - add player with first basic movement
    - add prototype level to test movement
    - add collision detection for player to set velocity 0
    - add autoload Glob for various future globals
      - add debug_panel ui

---
+++ entities/player/player.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+class_name Player
+
+const MAX_SPEED: float = 400
+const SKID_SPEED: float = 800
+const ACCELERATION: float = 500
+const START_SPEED: float = 1200
+
+var direction: Vector2
+
+@onready var camera: Camera2D = $Camera2D
+
+
+func _ready() -> void:
+	if is_multiplayer_authority():
+		Glob.player = self
+		camera.make_current()
+
+func _physics_process(delta: float) -> void:
+	_handle_movement(delta)
+	_handle_collisions()
+	Glob.debug_panel.add_property('Velocity', str(velocity), 2)
+	move_and_slide()
+
+
+func _handle_movement(delta: float) -> void:
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	if not direction.is_zero_approx():
+		# Init first move
+		if velocity.length() < START_SPEED * delta:
+			velocity = direction * START_SPEED * delta
+		# Handle move acceleration
+		else:
+			for axis: StringName in ["x", "y"]:
+				velocity[axis] += direction[axis] * ACCELERATION * delta
+	else:
+		for axis: StringName in ["x", "y"]:
+			velocity[axis] = move_toward(velocity[axis], 0, SKID_SPEED * delta)
+
+	# Prevent too fast movement
+	velocity = velocity.limit_length(MAX_SPEED)
+
+
+func _handle_collisions() -> void:
+	# Reset velocity when hitting wall straight on.
+	if direction.is_zero_approx(): return
+	for i: int in get_slide_collision_count():
+		var col: KinematicCollision2D = get_slide_collision(i)
+		var col_normal: Vector2 = col.get_normal()
+		var angle_to_col: float = rad_to_deg(col_normal.angle_to(direction))
+		if (angle_to_col > 179 and angle_to_col < 181) or (angle_to_col < -179 and angle_to_col > -181):
+			print(col.get_normal())
+			for axis: StringName in ["x", "y"]:
+				if col_normal[axis] > 0.0001 or col_normal[axis] < 0.0001:
+					velocity[axis] = 0.0
---
+++ services/debug_panel.gd
@@ -0,0 +1,29 @@
+extends PanelContainer
+class_name DebugPanel
+
+@onready var property_container: VBoxContainer = $VBoxContainer
+
+func _ready() -> void:
+	Glob.debug_panel = self
+	visible = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_debug_toggle"):
+		visible = !visible
+
+func _process(delta: float) -> void:
+	if not visible: return
+	add_property("FPS", "%.2f" % (1.0/delta), 0)
+
+
+## Adds or replaces given debug property.
+func add_property(title: StringName, value: StringName, order: int = -1) -> void:
+	var target: Label = property_container.find_child(title, true, false)
+	if not target:
+		target = Label.new()
+		property_container.add_child(target)
+		target.name = title
+
+	target.text = "%s: %s" % [target.name, value]
+	if order > -1:
+		property_container.move_child(target, mini(order, property_container.get_children().size()))
