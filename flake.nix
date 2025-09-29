06a7061d7a0228444af7bf2ff5c50a81b2f7430a
Dokkae6949 <finnliry@gmail.com>
2025-09-29 11:15:39 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Development environment for Godot";
+
+  inputs = {
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { system, pkgs, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "godot";
+
+          nativeBuildInputs = with pkgs; [
+            godot
+          ];
+        };
+      };
+    };
+}
