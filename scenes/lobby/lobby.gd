0f4698a8e817496e195fdc8793e80eef40a64def
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-10-02 16:16:09 +0200

    Update lobby UI and camera interpolation mode
    Refactored lobby.gd to use a unified refresh method for peer updates and added a lobby name label. Changed Camera2D's physics_interpolation_mode in lobby_manager.tscn from 1 to 2.

--- scenes/lobby/lobby.gd
+++ scenes/lobby/lobby.gd
@@ -2,19 +2,26 @@ extends CanvasLayer
 
 @onready var playerlist: VBoxContainer = $CenterContainer/VBoxContainer/Playerlist
 @onready var start_button: Button = $CenterContainer/VBoxContainer/StartButton
+@onready var lobby_name: RichTextLabel = $CenterContainer/VBoxContainer/LobbyName
 
 
 func _ready() -> void:
-	NetworkService.peer_connected.connect(_on_peers_changed)
-	NetworkService.peer_disconnected.connect(_on_peers_changed)
+	NetworkService.peer_connected.connect(refresh)
+	NetworkService.peer_disconnected.connect(refresh)
 	start_button.disabled = not multiplayer.is_server()
+	refresh(multiplayer.get_unique_id())
 
 
-func _on_peers_changed(_id: int) -> void:
+func refresh(_id: int) -> void:
+	var ip_address: StringName = IP.resolve_hostname(OS.get_environment("HOSTNAME"), IP.TYPE_IPV4)
+	if not ip_address:
+		ip_address = "127.0.0.1"
+	#lobby_name.text = "IP: %s:%s" % [ip_address, multiplayer.]
+
 	for child: Node in playerlist.get_children():
 		child.queue_free()
 
-	for peer: int in multiplayer.get_peers():
+	for peer: int in PackedInt32Array([multiplayer.get_unique_id()]) + multiplayer.get_peers():
 		var new_label: Label = Label.new()
 		new_label.text = str(peer)
 		playerlist.add_child(new_label)
