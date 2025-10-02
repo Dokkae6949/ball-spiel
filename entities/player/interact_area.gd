2f40e8052a0dc74001e51308fc7bd789de9a9873
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-10-02 10:55:39 +0200

    add ability to kick the ball

---
+++ entities/player/interact_area.gd
@@ -0,0 +1,22 @@
+extends Area2D
+class_name InteractArea
+
+
+const KICK_FORCE: float = 30000
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("player_interact"):
+		_on_interact()
+
+
+func _on_interact() -> void:
+	var ball: Ball
+	for body: Node2D in get_overlapping_bodies():
+		if body is Ball:
+			ball = body
+			break
+
+	if not ball: return
+
+	print("kick!")
+	ball.apply_central_impulse(Glob.player.global_position.direction_to(ball.global_position) * KICK_FORCE)
