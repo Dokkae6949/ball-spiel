1fe6b2bdbeb34ff364728c3548d87e4a7e5cd18a
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-10-03 20:09:19 +0200

    add sync for kicking ball

--- entities/player/interact_area.gd
+++ entities/player/interact_area.gd
@@ -3,12 +3,15 @@ class_name InteractArea
 
 
 const KICK_FORCE: float = 30000
+@export var player: Player
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("player_interact"):
-		_on_interact()
+		_on_interact.rpc_id(1)
 
 
+@rpc("any_peer", "call_local")
 func _on_interact() -> void:
 	var ball: Ball
 	for body: Node2D in get_overlapping_bodies():
@@ -17,4 +20,4 @@ func _on_interact() -> void:
 			break
 
 	if not ball: return
-	ball.apply_central_impulse(Glob.player.global_position.direction_to(ball.global_position) * KICK_FORCE)
+	ball.apply_central_impulse(player.global_position.direction_to(ball.global_position) * KICK_FORCE)
